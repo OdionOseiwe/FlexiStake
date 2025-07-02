183561c56a0ae7aad1973e1c007edfa8ba1606fe
OdionOseiwe <oseiweifebhor@gmail.com>
2025-07-02 21:33:55 +0100

    working on the homepage

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+        <h1>Welcome to FlexiStake</h1>
+        <p>Your one-stop solution for all your flexible stake needs</p>
+        <p>Get Started by exploring our plaform and unlocking the power of flexible stakes.</p>
+        <p> Join our community and discover the benefits of flexible stakes today.</p>
+        <button>Get Started</button>
+    </div>
+  )
+}
---
+++ frontend/src/pages/StakePage.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function StakePage() {
+  return (
+    <div>
+        <div>
+            <button>Connect wallet</button>
+        </div>
+
+        <div>
+            <div>
+                stake
+            </div>
+            <div>
+                <div>
+                    FlexiStake Stats
+                </div>
+                <div>
+                    Assets Restaked
+                </div>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
